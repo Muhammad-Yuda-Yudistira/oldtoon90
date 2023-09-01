460a6729e448a523f6730a6e07d46c024c670631
Muhammad-Yuda-Yudistira <yudistira22112022@gmail.com>
2023-09-01 22:41:17 +0700

    add: logo: part 2. add: bootstrap, redesign: navbar: process

--- ui/templates/nav.php
+++ ui/templates/nav.php
@@ -1,45 +1,49 @@
-<nav class="nav-main">
-    <ul>
-        <div class="logo">
-            <a href="<?= $baseurl ?>ui/home.php" class="close">
-                <li>Home</li>
-            </a>
-        </div>
-        <div class="menu">
-            <a href="<?= $baseurl ?>ui/user/main.php">
-                <li>Film</li>
-            </a>
-            <a href="<?= $baseurl ?>ui/fanart/image.php" class="close">
-                <li>Image</li>
-            </a>
-            <a href="<?= $baseurl ?>ui/fanart/komik.php" class="close">
-                <li>Komik</li>
-            </a>
-        </div>
-        <div class="daftar">
-            <?php if(!isset($_SESSION['login'])): ?>
-                <a href="<?= $baseurl ?>ui/user/login.php">
-                    <li>Login</li>
-                </a>
-                <a href="<?= $baseurl ?>ui/user/signup.php">
-                    <li>Sign Up</li>
-                </a>
-            <?php endif; ?>
-
-            <?php if(isset($_SESSION['login'])): ?>
-
-                <a href="<?= $baseurl ?>ui/user/logout.php">
-                    <li>logout</li>
-                </a>
-
-                <a href="" class="box-profile">
-                    <li class="profile">
-                        <img src="<?= $baseurl ?>images/users/blank-profile.webp" title="<?= $_SESSION['username'] ?>">
-                    </li>
-                </a>
-
-            <?php endif; ?>
-
-        </div>
+<nav class="navbar navbar-expand-lg bg-danger">
+  <div class="container justify-content-between">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?= $baseurl ?>ui/home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?= $baseurl ?>ui/user/main.php">Film</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true" href="<?= $baseurl ?>ui/fanart/image.php" >Image</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true" href="<?= $baseurl ?>ui/fanart/comic.php" >Comic</a>
+        </li>
     </ul>
+    </div>
+    <div class="collapse navbar-collapse ml-auto" id="navbarNav">
+        <?php if (!isset($_SESSION['login'])) :?>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?= $baseurl ?>ui/user/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= $baseurl ?>ui/user/signup.php">Signup</a>
+            </li>
+        </ul>
+        <?php endif; ?>
+
+        <?php if (isset($_SESSION['login'])) :?>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="<?= $baseurl ?>ui/user/logout.php">Logout</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?= $baseurl ?>ui/user/signup.php">
+                <img src="<?= $baseurl ?>images/users/blank-profile.webp" title="<?= $_SESSION['username'] ?>" class="img-fluid img-thumbnail rounded-circle" width="30">
+              </a>
+            </li>
+        </ul>
+        <?php endif; ?>
+    </div>
+  </div>
 </nav>
