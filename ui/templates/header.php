ae9845952d54e5b243355cf8a4ddeee54492c1b3
Muhammad-Yuda-Yudistira <yudistira22112022@gmail.com>
2023-09-15 18:27:56 +0700

    style(film pages) Mengganti UI Halaman
    [200~Mengganti ui dengan bootstrap untuk tampilan yang lebih konsisten dan menarik
    ~ belum selesesai harus di tunda dahulu. yang sudah dikerjakan: membuat design system, mengubah navbar dan nav second, menambahkan card
    
    BREAKING CHANGE: Peruban ini memerlukn penyesuaian css dan design system

--- ui/templates/header.php
+++ ui/templates/header.php
@@ -34,6 +34,8 @@
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="<?= $baseurl ?>css/design-system.css">
 
     <link rel="stylesheet" href="<?= $baseurl ?>css/templates/nav.css">  
     <link rel="stylesheet" href="<?= $baseurl ?>css/templates/nav-second.css">  
@@ -41,7 +43,6 @@
 
     <link rel="stylesheet" href="<?= $baseurl ?>css/templates/footer.css">  
     <link rel="stylesheet" href="<?= $baseurl ?>css/style.css">
-    <link rel="stylesheet" href="<?= $baseurl ?>css/design-system.css">
     <link rel="stylesheet" href="<?= $baseurl ?>css/umum/items.css">
 
     <link rel="stylesheet" href="<?= $baseurl ?>css/templates/user/reaction-icon.css">
@@ -63,4 +64,4 @@
 </head>
 <body>
 
-    <?php require_once "nav-second.php" ?>
+    <!-- <?php include "nav-second.php" ?> -->
--- ui/user/main.php
+++ ui/user/main.php
@@ -39,77 +39,116 @@ if(isset($_COOKIE['remember']))
 <?php include "../templates/header.php" ?>
 <?php include "../templates/nav.php" ?>
 
-    <div class="container" style="overflow-x: hidden;">
-        
-        <div class="content" style="width: 80vw;">
-
-            <div class="title">
-                <h1>Daftar Film</h1>
-            </div>
-            
-            <div class="box-film">
-
-                <?php foreach($data as $d): ?>
-                    
-                    <?php $id = $d['id'] ?>
-                    <?php $localInfo = query("SELECT * FROM tayang_local WHERE title_id=$id"); ?>
-
-                    <a href="<?= $baseurl; ?>ui/detail.php?id=<?= $id ?>">
-                        <div class="card">
-                            <div class="img">
-                                <img src="<?= $baseurl . $d['cover'] ?>">
-                            </div>
-                            <ul class="card-fill">
-                                <li><span class="list-title">Judul:</span> <?= $d['title'] ?></li>
-                                <li><span class="list-title">Episode:</span> <?= $d['episode'] ?></li>
-                                <li><span class="list-title">Channel:</span> 
-                                    <?= $localInfo[0]['channel'] ?>,
-                                </li>
-                                <li><span class="list-title">Tahun:</span> 
-                                    <?= $localInfo[0]['tahun'] ?>,
-                                </li>
-                            </ul>
-                        </div>
-                    </a>
-
-                <?php endforeach; ?>
-            </div>
-            <div class="pagination">
-
-                <?php if($totalPages > 1): ?>
-                    <a href="">
-                        <span>&laquo;</span>
-                    </a>
-                
-                    <?php 
-                    if(isset($_GET['page']))
-                    {
-                        $activePage = $_GET['page'];
-                    }
-                    else
-                    {
-                        $activePage = 1;
-                    }
-                    ?>
-                    <?php for($i = 0; $i < $totalPages; $i++): ?>
-                        
-                        <a href="<?= $baseurl; ?>ui/user/main.php?page=<?= $page ?>" style="font-weight: <?= $page == $activePage ? 'bold' : ''; ?>">
-                            <span class="page"><?= $page ?></span>
-                        </a>
-                        
-                        <?php $page++; ?>
-                    <?php endfor; ?>
-
-                    <a href="">
-                        <span>&raquo;</span>
-                    </a>
-                <?php endif ?>
-                
-            </div>
+  <div id="film" class="film">
+    <div class="container mycontainer">
+      <div class="row text-center">
+        <div class="col">
+          <h1>Daftar Film</h1>
         </div>
+      </div>
+      <div class="row justify-content-end">
+        <div class="col">
+          <ul>
+          <?php foreach($data as $d): ?>
+          <div class="row">
+            <div class="col-md-3">
+                      
+              <?php $id = $d['id'] ?>
+              <?php $localInfo = query("SELECT * FROM tayang_local WHERE title_id=$id"); ?>
+  
+              <li>
+                <a href="<?= $baseurl; ?>ui/detail.php?id=<?= $id ?>">
+                  <div class="card" style="width: 18rem;">
+                    <img src="<?= $baseurl . $d['cover'] ?>" class="card-img-top" alt="<?= $baseurl . $d['title'] ?>">
+                    <div class="card-img-overlay">
+                    <p class="card-text"><small><?= $d['episode'] ?></small></p>
+                  </div>
+                    <div class="card-body">
+                        <p class="card-text"><?= $d['title'] ?></p>
+                        <p class="card-text"><?= $localInfo[0]['channel'] ?></p>
+                        <p class="card-text"><?= $localInfo[0]['tahun'] ?></p>
+                    </div>
+                  </div>
+                </a>
+              </li>
+    
+            </div>
+          </div>
+          <?php endforeach; ?>
+          </ul>
+        </div>
+      </div>
 
-        <?php require_once '../components/sidebar.php'; ?>
+      <div class="content" style="width: 80vw;">
+
+          <div class="title">
+              <h1>Daftar Film</h1>
+          </div>
+          
+          <div class="box-film">
+
+              <?php foreach($data as $d): ?>
+                  
+                  <?php $id = $d['id'] ?>
+                  <?php $localInfo = query("SELECT * FROM tayang_local WHERE title_id=$id"); ?>
+
+                  <a href="<?= $baseurl; ?>ui/detail.php?id=<?= $id ?>">
+                      <div class="card">
+                          <div class="img">
+                              <img src="<?= $baseurl . $d['cover'] ?>">
+                          </div>
+                          <ul class="card-fill">
+                              <li><span class="list-title">Judul:</span> <?= $d['title'] ?></li>
+                              <li><span class="list-title">Episode:</span> <?= $d['episode'] ?></li>
+                              <li><span class="list-title">Channel:</span> 
+                                  <?= $localInfo[0]['channel'] ?>,
+                              </li>
+                              <li><span class="list-title">Tahun:</span> 
+                                  <?= $localInfo[0]['tahun'] ?>,
+                              </li>
+                          </ul>
+                      </div>
+                  </a>
+
+              <?php endforeach; ?>
+          </div>
+          <div class="pagination">
+
+              <?php if($totalPages > 1): ?>
+                  <a href="">
+                      <span>&laquo;</span>
+                  </a>
+              
+                  <?php 
+                  if(isset($_GET['page']))
+                  {
+                      $activePage = $_GET['page'];
+                  }
+                  else
+                  {
+                      $activePage = 1;
+                  }
+                  ?>
+                  <?php for($i = 0; $i < $totalPages; $i++): ?>
+                      
+                      <a href="<?= $baseurl; ?>ui/user/main.php?page=<?= $page ?>" style="font-weight: <?= $page == $activePage ? 'bold' : ''; ?>">
+                          <span class="page"><?= $page ?></span>
+                      </a>
+                      
+                      <?php $page++; ?>
+                  <?php endfor; ?>
+
+                  <a href="">
+                      <span>&raquo;</span>
+                  </a>
+              <?php endif ?>
+              
+          </div>
+      </div>
+
+      <?php require_once '../components/sidebar.php'; ?>
 
     </div>
+  </div>
 
     <?php require '../templates/footer.php'; ?>
